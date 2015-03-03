fc21931aa2afbd10757b4fb0e00ec9dde11791d5
Matth_eu <matthew@matth.eu>
2015-03-03 13:59:46 +0000

    color schemes

---
+++ admin-color-schemes.php
@@ -0,0 +1,107 @@
+<?php
+/**
+Plugin Name: Matts Admin Color Schemes
+Plugin URI: http://matth.eu
+Description: Yet Even more admin color schemes
+Version: 1.0
+Author: Mattheu
+Author URI: http://matth.eu
+Text Domain: admin_schemes
+Domain Path: /languages
+*/
+
+/*
+Copyright 2013 Kelly Dwan, Mel Choyce, Dave Whitley, Kate Whitley
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+class MACS_Color_Schemes {
+
+	/**
+	 * List of colors registered in this plugin.
+	 *
+	 * @since 1.0
+	 * @access private
+	 * @var array $colors List of colors registered in this plugin.
+	 *                    Needed for registering colors-fresh dependency.
+	 */
+	private $colors = array(
+		'matt', 'matt2', 'matt3'
+	);
+
+	function __construct() {
+		add_action( 'admin_enqueue_scripts', array( $this, 'load_default_css' ) );
+		add_action( 'admin_init' , array( $this, 'add_colors' ) );
+	}
+
+	/**
+	 * Register color schemes.
+	 */
+	function add_colors() {
+		$suffix = is_rtl() ? '-rtl' : '';
+
+		wp_admin_css_color(
+			'matt', __( 'Matt 1', 'admin_schemes' ),
+			plugins_url( "matt/colors$suffix.css", __FILE__ ),
+			array( '#353129', '#484A47', '#c1d973', '#81AFB5' ),
+			array( 'base' => '#ECEBCA', 'focus' => '#c1d973', 'current' => '#c1d973' )
+		);
+
+		wp_admin_css_color(
+			'matt2', __( 'Matt 2', 'admin_schemes' ),
+			plugins_url( "matt2/colors$suffix.css", __FILE__ ),
+			array( '#52504C', '#65625d', '#cb992f', '#cb2f2f' ),
+			array( 'base' => '#65625d', 'focus' => '#cb992f', 'current' => '#cb992f' )
+		);
+
+		wp_admin_css_color(
+			'matt3', __( 'Matt 3', 'admin_schemes' ),
+			plugins_url( "matt3/colors$suffix.css", __FILE__ ),
+			array( '#821116', '#9C0D13', '#E8A41E', '#E5E3DE' ),
+			array( 'base' => '#ECEBCA', 'focus' => '#cb992f', 'current' => '#cb992f' )
+		);
+
+		wp_admin_css_color(
+			'matt4', __( 'Matt 4', 'admin_schemes' ),
+			plugins_url( "matt4/colors$suffix.css", __FILE__ ),
+			array( '#0F1F27', '#193441', '#7FAB6F', '#437D9A' ),
+			array( 'base' => '#ECEBCA', 'focus' => '#cb992f', 'current' => '#cb992f' )
+		);
+
+	}
+
+	/**
+	 * Make sure core's default `colors.css` gets enqueued, since we can't
+	 * @import it from a plugin stylesheet. Also force-load the default colors
+	 * on the profile screens, so the JS preview isn't broken-looking.
+	 */
+	function load_default_css() {
+
+		global $wp_styles, $_wp_admin_css_colors;
+
+		$color_scheme = get_user_option( 'admin_color' );
+
+		$scheme_screens = apply_filters( 'acs_picker_allowed_pages', array( 'profile', 'profile-network' ) );
+		if ( in_array( $color_scheme, $this->colors ) || in_array( get_current_screen()->base, $scheme_screens ) ){
+			$wp_styles->registered[ 'colors' ]->deps[] = 'colors-fresh';
+		}
+
+	}
+
+}
+global $acs_colors;
+$acs_colors = new MACS_Color_Schemes;
+
